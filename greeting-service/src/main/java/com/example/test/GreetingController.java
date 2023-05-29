fffab367995a4372b4805f3624a0e67d3bd76f01
Felipe <felipeschutz2025@hotmail.com>
2023-05-29 08:42:10 -0300

    :pencil:

--- greeting-service/src/main/java/com/example/test/GreetingController.java
+++ greeting-service/src/main/java/com/example/test/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,15 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "%s, %s!";
     private  final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private Environment environment;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(template, environment.getProperty("greeting-service.greeting"), name));
     }
 
 }
